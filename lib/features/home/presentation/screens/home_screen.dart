d30e6e6d3bdcda8ea691ab88a4883705fa9c3d36
nghia.vuong <nghia.vuong@defide-x.com>
2024-09-11 16:12:04 +0700

    remove home screen widget

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,8 +1,11 @@
-import 'package:demo_flutter/features/home/presentation/screens/home_screen_widget.dart';
+import 'package:demo_flutter/features/user/presentation/screens/user_list_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
+
+  // lay data lam trung tam
 
   final String title;
 
@@ -33,7 +36,36 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     print('BUILD: build, mounted: $mounted');
-    return HomeScreenWidget();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Home"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.all(1.0),
+              child: Text(
+                'This is home page Minh',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.to(const UserListScreen(), arguments: "Data received from first screen");
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(7), // <-- Radius
+                ),
+              ),
+              child: const Text('Example Demo User List'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   @override
